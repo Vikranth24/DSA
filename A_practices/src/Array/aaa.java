7a00a3bea03639f65fa7efccccb254023a817c40
vikra <vikra@HP_WIN_4>
2024-01-24 16:00:36 +0530

    24-01-2024

--- A_practices/src/Array/aaa.java
+++ A_practices/src/Array/aaa.java
@@ -6,9 +6,60 @@ public class aaa {
 
 	 public static void main(String[] args)
 	    {
-	        int[] arr = { 3, 2, 5, 6, 1, 4 };
-	        
-	         
-	          
-}
-}
+		 
+		 int arr[]= {1,4,20,3,10,5};
+		 int sum=33;
+		 int n=arr.length;
+		
+		 for (int i=0;i<n;i++)
+		 {
+			 if(arr[i]==sum)
+			 {
+				 System.out.println(i +" is the index");
+			 }
+			 int total=arr[i];
+			 
+			 for (int j=i+1;j<n;j++)
+			 {
+				 total=total+arr[j];
+				 
+				 if (total ==sum)
+				 {
+					 System.out.println(i+" to "+j);
+				 }
+				 else if(total >sum)
+				 {
+					 break;
+				 }
+				 
+			 }
+			 
+		 }
+		 
+		 
+		
+		 
+		 
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
